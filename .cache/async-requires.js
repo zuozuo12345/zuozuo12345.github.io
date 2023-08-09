54fdb1f8aa1b91f7f08b09d9bb4f3f5351fb866e
Zuo Yue <115383220+zuozuo12345@users.noreply.github.com>
2023-08-09 15:19:28 +0800

    update

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -1,24 +1,11 @@
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=default" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-404-js": () => import("./../../../src/pages/404.js?export=default" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-archive-js": () => import("./../../../src/pages/archive.js?export=default" /* webpackChunkName: "component---src-pages-archive-js" */),
-  "component---src-pages-index-js": () => import("./../../../src/pages/index.js?export=default" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-pensieve-index-js": () => import("./../../../src/pages/pensieve/index.js?export=default" /* webpackChunkName: "component---src-pages-pensieve-index-js" */),
-  "component---src-pages-pensieve-tags-js": () => import("./../../../src/pages/pensieve/tags.js?export=default" /* webpackChunkName: "component---src-pages-pensieve-tags-js" */),
-  "component---src-templates-post-js": () => import("./../../../src/templates/post.js?export=default" /* webpackChunkName: "component---src-templates-post-js" */),
-  "component---src-templates-tag-js": () => import("./../../../src/templates/tag.js?export=default" /* webpackChunkName: "component---src-templates-tag-js" */)
-}
-
-
-
-exports.head = {
-  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=head" /* webpackChunkName: "component---cache-dev-404-page-jshead" */),
-  "component---src-pages-404-js": () => import("./../../../src/pages/404.js?export=head" /* webpackChunkName: "component---src-pages-404-jshead" */),
-  "component---src-pages-archive-js": () => import("./../../../src/pages/archive.js?export=head" /* webpackChunkName: "component---src-pages-archive-jshead" */),
-  "component---src-pages-index-js": () => import("./../../../src/pages/index.js?export=head" /* webpackChunkName: "component---src-pages-index-jshead" */),
-  "component---src-pages-pensieve-index-js": () => import("./../../../src/pages/pensieve/index.js?export=head" /* webpackChunkName: "component---src-pages-pensieve-index-jshead" */),
-  "component---src-pages-pensieve-tags-js": () => import("./../../../src/pages/pensieve/tags.js?export=head" /* webpackChunkName: "component---src-pages-pensieve-tags-jshead" */),
-  "component---src-templates-post-js": () => import("./../../../src/templates/post.js?export=head" /* webpackChunkName: "component---src-templates-post-jshead" */),
-  "component---src-templates-tag-js": () => import("./../../../src/templates/tag.js?export=head" /* webpackChunkName: "component---src-templates-tag-jshead" */)
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": () => import("./../../caches/gatsby-plugin-offline/app-shell.js" /* webpackChunkName: "component---cache-caches-gatsby-plugin-offline-app-shell-js" */),
+  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-archive-js": () => import("./../../../src/pages/archive.js" /* webpackChunkName: "component---src-pages-archive-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-pensieve-index-js": () => import("./../../../src/pages/pensieve/index.js" /* webpackChunkName: "component---src-pages-pensieve-index-js" */),
+  "component---src-pages-pensieve-tags-js": () => import("./../../../src/pages/pensieve/tags.js" /* webpackChunkName: "component---src-pages-pensieve-tags-js" */),
+  "component---src-templates-post-js": () => import("./../../../src/templates/post.js" /* webpackChunkName: "component---src-templates-post-js" */),
+  "component---src-templates-tag-js": () => import("./../../../src/templates/tag.js" /* webpackChunkName: "component---src-templates-tag-js" */)
 }
 
